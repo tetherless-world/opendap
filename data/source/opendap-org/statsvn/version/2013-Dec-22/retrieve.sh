e3b21e6bd03dbcb36c05274928e18749d5056893
Tim Lebo <timrdf@gmail.com>
2013-12-23 03:22:09 +0000

    retrieval trigger for statsvn of https://scm.opendap.org/svn/tags

---
+++ data/source/opendap-org/statsvn/version/2013-Dec-22/retrieve.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+mkdir manual &> /dev/null
+
+log="../../../opendap/version/svn/manual/tags.log"
+if [[ ! -e $log ]]; then
+   echo "error: need $log"
+   exit 1
+else
+   pushd manual &> /dev/null
+      echo repo-statistics.xml
+      if [[ ../$log -nt repo-statistics.xml ]]; then
+         java -jar ../../../src/statsvn-0.7.0/statsvn.jar -xml                 ../$log ../source/tags
+      fi
+      echo rendered/index.html
+      if [[ ../$log -nt rendered/index.html ]]; then
+         java -jar ../../../src/statsvn-0.7.0/statsvn.jar -output-dir rendered ../$log ../source/tags
+      fi
+   popd &> /dev/null
+fi
