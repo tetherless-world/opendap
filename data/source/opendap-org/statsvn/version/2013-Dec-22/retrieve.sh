0cf8f126566b3738f5fef8daf24522eb6347b9b1
Tim Lebo <timrdf@gmail.com>
2013-12-23 03:51:09 +0000

    statsvn retrieveal trigger

--- data/source/opendap-org/statsvn/version/2013-Dec-22/retrieve.sh
+++ data/source/opendap-org/statsvn/version/2013-Dec-22/retrieve.sh
@@ -8,13 +8,15 @@ if [[ ! -e $log ]]; then
    exit 1
 else
    pushd manual &> /dev/null
+      tags="../../../../opendap/version/svn/source/tags"
+
       echo repo-statistics.xml
       if [[ ../$log -nt repo-statistics.xml ]]; then
-         java -jar ../../../src/statsvn-0.7.0/statsvn.jar -xml                 ../$log ../source/tags
+         java -jar ../../../src/statsvn-0.7.0/statsvn.jar -xml                 ../$log $tags
       fi
       echo rendered/index.html
       if [[ ../$log -nt rendered/index.html ]]; then
-         java -jar ../../../src/statsvn-0.7.0/statsvn.jar -output-dir rendered ../$log ../source/tags
+         java -jar ../../../src/statsvn-0.7.0/statsvn.jar -output-dir rendered ../$log $tags
       fi
    popd &> /dev/null
 fi
