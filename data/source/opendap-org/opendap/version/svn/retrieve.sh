f0b0f7678d964b1208bfa5a56f7dfbd8e42e44bf
Tim L <timrdf@gmail.com>
2013-12-28 14:26:14 -0500

    avoid svn checkout

--- data/source/opendap-org/opendap/version/svn/retrieve.sh
+++ data/source/opendap-org/opendap/version/svn/retrieve.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 for portion in tags trunk; do
-   if [[ ! -e source/$portion ]]; then
-      pushd source &> /dev/null
-         echo svn co https://scm.opendap.org/svn/$portion/
-              svn co https://scm.opendap.org/svn/$portion/
-      popd &> /dev/null
+   echo manual/$portion.log
+   if [[ ! -e manual/$portion.log ]]; then
+      if [[ ! -e source/$portion ]]; then
+         pushd source &> /dev/null
+            echo svn co https://scm.opendap.org/svn/$portion/
+                 svn co https://scm.opendap.org/svn/$portion/
+         popd &> /dev/null
+      else
+         pushd source/$portion &> /dev/null
+            echo svn update
+                 svn update
+         popd &> /dev/null
+      fi
    else
-      pushd source/$portion &> /dev/null
-         echo svn update
-              svn update
-      popd &> /dev/null
+      echo "Avoiding SVN checkout b/c manual/$portion.log already exists."
    fi
 done
