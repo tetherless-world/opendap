0467e2acfb7c915ef2555ab2aad212478d07ce9b
Tim L <timrdf@gmail.com>
2013-12-28 12:46:54 -0500

    convert trigger

--- data/source/opendap-org/opendap/version/svn/convert.sh
+++ data/source/opendap-org/opendap/version/svn/convert.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-saxon.sh ../../src/grddl.xsl xml ttl -v `cr-sdv.sh --attribute-value` -in manual/tags.log 
+mkdir automatic &> /devnull
+
+for portion in tags trunk; do
+   if [[ manual/$portion.log -nt automatic/$portion.log.ttl ]]; then
+      saxon.sh ../../src/grddl.xsl xml ttl -v `cr-sdv.sh --attribute-value` -in manual/$portion.log > automatic/$portion.log.ttl
+   fi
+done
