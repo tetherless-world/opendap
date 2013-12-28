712ff3067ac9785c82b79c6796a384d32c9a9577
Tim L <timrdf@gmail.com>
2013-12-28 14:29:28 -0500

    avoiding svn checkout

--- data/source/opendap-org/opendap/version/svn/prepare.sh
+++ data/source/opendap-org/opendap/version/svn/prepare.sh
@@ -4,9 +4,11 @@ mkdir manual &> /dev/null
 
 for portion in tags trunk; do
    echo manual/$portion.log
-   if [[ source/$portion -nt manual/$portion.log ]]; then
+   if [[ -e source/$portion && source/$portion -nt manual/$portion.log ]]; then
       pushd source/$portion
          svn log -v --xml > ../../manual/$portion.log
       popd
+   else
+      echo "   (Skipping svn log because svn repository is not checked out.)"
    fi
 done
