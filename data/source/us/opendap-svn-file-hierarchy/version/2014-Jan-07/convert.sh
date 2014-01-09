54aa6e7e084b4faed56c526048e9e1140579f4a1
Tim Lebo <timrdf@gmail.com>
2014-01-09 02:30:33 +0000

    added --clean to triggers, updated SVN File query

--- data/source/us/opendap-svn-file-hierarchy/version/2014-Jan-07/convert.sh
+++ data/source/us/opendap-svn-file-hierarchy/version/2014-Jan-07/convert.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ "$1" == '--clean' ]]; then
+   rm -f automatic/svn-files.bte.ttl
+   exit
+fi
+
 mkdir automatic &> /dev/null
 
 python $DATAFAQS_HOME/services/sadi/faqt/naming/between-the-edges.py source/svn-files.rq.ttl text/turtle automatic/svn-files.bte.ttl
--- data/source/us/opendap-svn-file-hierarchy/version/2014-Jan-07/retrieve.sh
+++ data/source/us/opendap-svn-file-hierarchy/version/2014-Jan-07/retrieve.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+if [[ "$1" == '--clean' ]]; then
+   rm -f source/svn-files.rq.ttl*
+   exit
+fi
+
 cache-queries.sh $CSV2RDF4LOD_PUBLISH_SPARQL_ENDPOINT -o ttl -q manual/svn-files.rq -od source/
