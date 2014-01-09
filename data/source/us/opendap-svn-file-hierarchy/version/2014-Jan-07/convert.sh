fb41126ba5f9afcb23e43194bdbf607c99cd9d5c
Tim Lebo <timrdf@gmail.com>
2014-01-09 02:33:24 +0000

    added publish and justify to conversion trigger

--- data/source/us/opendap-svn-file-hierarchy/version/2014-Jan-07/convert.sh
+++ data/source/us/opendap-svn-file-hierarchy/version/2014-Jan-07/convert.sh
@@ -8,3 +8,10 @@ fi
 mkdir automatic &> /dev/null
 
 python $DATAFAQS_HOME/services/sadi/faqt/naming/between-the-edges.py source/svn-files.rq.ttl text/turtle automatic/svn-files.bte.ttl
+justify.sh source/svn-files.rq.ttl automatic/svn-files.bte.ttl https://github.com/timrdf/DataFAQs/blob/master/services/sadi/faqt/naming/between-the-edges.py
+
+if [[ "$CSV2RDF4LOD_PUBLISH" == 'true' ]]; then
+   aggregate-source-rdf.sh automatic/svn-files.bte.ttl
+else
+   echo "Avoiding publishing b/c \$CSV2RDF4LOD_PUBLISH is not 'true'; set to 'true' to publish."
+fi
