0a5fd1d992c383319c2e454cd42d705932d37293
Tim Lebo <timrdf@gmail.com>
2014-01-09 02:15:56 +0000

    added justification in conversion trigger

--- data/source/opendap-org/opendap/version/svn/convert.sh
+++ data/source/opendap-org/opendap/version/svn/convert.sh
@@ -9,6 +9,7 @@ mkdir automatic &> /dev/null
 for portion in tags trunk; do
    if [[ manual/$portion.log -nt automatic/$portion.log.ttl || ../../src/grddl.xsl -nt automatic/$portion.log.ttl ]]; then
       saxon.sh ../../src/grddl.xsl xml ttl -v `cr-sdv.sh --attribute-value` -in manual/$portion.log > automatic/$portion.log.ttl
+      justify.sh manual/$portion.log automatic/$portion.log.ttl ../../src/grddl.xsl
       say `valid-rdf.sh automatic/$portion.log.ttl`
    fi
 done
