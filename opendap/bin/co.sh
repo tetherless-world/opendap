82aa517a42520002c7390ef122818113c82637fb
Tim Lebo <timrdf@gmail.com>
2014-01-13 18:19:49 +0000

    opendap install script - comments

--- opendap/bin/co.sh
+++ opendap/bin/co.sh
@@ -4,14 +4,22 @@
 #3>    prov:wasDerivedFrom   <https://github.com/tetherless-world/opendap/issues/7>;
 #3> .
 
-conf="$1"
+configuration="$1"
+# e.g. https://github.com/tetherless-world/opendap/blob/master/opendap/conf/hyrax-1.8_1.txt
 
 base="https://scm.opendap.org/svn/tags"
 
-if [[ -e "$conf" ]]; then
-   for i in `cat $conf`; do
-      comp=`echo $i | cut -d'/' -f1`
-      vers=`echo $i | cut -d'/' -f2`
+if [[ -e "$configuration" ]]; then
+   #
+   # OPeNDAP Modules are listed at:
+   # https://docs.google.com/spreadsheet/ccc?key=0An84UEjofnaydFRrUF9YWk03Y3NHNjJqUEg0NUhUZXc#gid=0
+   #
+   for $module in `cat $configuration`; do
+      #
+      # e.g. module = 'libdap/3.11.7'
+      #
+      comp=`echo $module | cut -d'/' -f1`
+      vers=`echo $module | cut -d'/' -f2`
       toget="${base}/${comp}/${vers}"
       svn co $toget $comp
    done
