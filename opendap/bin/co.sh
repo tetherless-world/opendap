b43eb30ae15f06a86080f952a72e9ac7f394a561
Tim Lebo <timrdf@gmail.com>
2014-01-13 18:02:52 +0000

    accept argument for config file - opendap installer

--- opendap/bin/co.sh
+++ opendap/bin/co.sh
@@ -1,12 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
+conf="$1"
 
 base="https://scm.opendap.org/svn/tags"
 
-for i in `cat versionsToGet`
-do
-	comp=`echo $i | cut -d'/' -f1`
-	vers=`echo $i | cut -d'/' -f2`
-	toget="${base}/${comp}/${vers}"
-	svn co $toget $comp
-done
-
+if [[ -e "$conf" ]]; then
+   for i in `cat $conf`; do
+      comp=`echo $i | cut -d'/' -f1`
+      vers=`echo $i | cut -d'/' -f2`
+      toget="${base}/${comp}/${vers}"
+      svn co $toget $comp
+   done
+fi
