ab7f466ed08129278f9a98bf8cf44349ffb3f41d
Tim Lebo <timrdf@gmail.com>
2014-01-16 21:55:37 +0000

    moving from test case to full-fledged versioned trigger

--- data/source/us/opendap-svn-file-hierarchy/version/2014-Jan-07/convert.sh
+++ data/source/us/opendap-svn-file-hierarchy/version/2014-Jan-07/convert.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+name="eg-nasa-shell"
+
 if [[ "$1" == '--clean' ]]; then
-   rm -f automatic/svn-files.bte.ttl
+   rm -f automatic/$name.bte.ttl
    exit
 fi
 
 mkdir automatic &> /dev/null
 
-python $DATAFAQS_HOME/services/sadi/faqt/naming/between-the-edges.py source/svn-files.rq.ttl text/turtle automatic/svn-files.bte.ttl
-justify.sh source/svn-files.rq.ttl automatic/svn-files.bte.ttl https://github.com/timrdf/DataFAQs/blob/master/services/sadi/faqt/naming/between-the-edges.py
+python $DATAFAQS_HOME/services/sadi/faqt/naming/between-the-edges.py source/$name.rq.ttl text/turtle automatic/$name.bte.ttl
+justify.sh source/$name.rq.ttl automatic/$name.bte.ttl https://github.com/timrdf/DataFAQs/blob/master/services/sadi/faqt/naming/between-the-edges.py
 
 if [[ "$CSV2RDF4LOD_PUBLISH" == 'true' ]]; then
-   aggregate-source-rdf.sh automatic/svn-files.bte.ttl
+   aggregate-source-rdf.sh automatic/$name.bte.ttl
 else
    echo "Avoiding publishing b/c \$CSV2RDF4LOD_PUBLISH is not 'true'; set to 'true' to publish."
 fi
