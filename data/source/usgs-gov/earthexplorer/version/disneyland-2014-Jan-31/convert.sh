a768117ae9bfbf9ad9c5f2e14ca0a16c577c3595
Tim L <timrdf@gmail.com>
2014-02-02 16:19:03 -0500

    -k 3

--- data/source/usgs-gov/earthexplorer/version/disneyland-2014-Jan-31/convert.sh
+++ data/source/usgs-gov/earthexplorer/version/disneyland-2014-Jan-31/convert.sh
@@ -4,5 +4,5 @@ source ~/*/utilities/netcdf/local-paths.sh
 
 mkdir -p automatic
 
-ncgen -o automatic/CA_OrangeCo_2011_000402.sample.cdl.nc manual/CA_OrangeCo_2011_000402.sample.cdl
+ncgen -k 3 -o automatic/CA_OrangeCo_2011_000402.sample.cdl.nc manual/CA_OrangeCo_2011_000402.sample.cdl
 ncdump automatic/CA_OrangeCo_2011_000402.sample.cdl.nc
